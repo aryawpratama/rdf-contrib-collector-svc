a3c992ac6b49d02df9a91fbcde7c25a7a3a4a444
aryawpratama <aryawpratama@gmail.com>
2025-03-06 20:28:47 +0700

    chore(Transport): implement webhook handler

--- transport/git_handler.go
+++ transport/git_handler.go
@@ -1,12 +1,14 @@
 package transport
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/google/go-github/v69/github"
 )
 
 func (t transport) GitWebhookHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.TODO()
 	payload, err := github.ValidatePayload(r, []byte(t.config.App.Secret))
 	if err != nil {
 		t.log.Error(err.Error())
@@ -14,7 +16,7 @@ func (t transport) GitWebhookHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(http.StatusText(500)))
 	}
 	event, _ := github.ParseWebHook(github.WebHookType(r), payload)
-	if err := t.usecase.HandleWebhook(event); err != nil {
+	if err := t.usecase.HandleWebhook(ctx, event); err != nil {
 		t.log.Error(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(http.StatusText(500)))
